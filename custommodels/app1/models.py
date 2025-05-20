cc47ac0a9188751bfad5e225a8618425346ec6af
kaustubhthakur02 <thakurkaustubh37@gmail.com>
2025-05-20 22:29:37 +0530

    Changes for the Custom model managers final
    Changes for the Custom model managers final

--- custommodels/app1/models.py
+++ custommodels/app1/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class ProductManager(models.Manager):        
+    def available(self):
+        return self.filter(available=True)
+    
+    def featured(self):
+        return self.available().filter(featured=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    stock = models.IntegerField(default=0)
+    featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    objects = models.Manager()  # The default manager
+    items = ProductManager()    # Our custom manager
+    
+    def __str__(self):
+        return self.name
--- custommodels/app1/views.py
+++ custommodels/app1/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Product
 
-# Create your views here.
+# BEFORE: In your views
+def product_list(request):
+    products = Product.objects.filter(available=True).order_by('-created_at')
+    featured = Product.objects.filter(available=True, featured=True)
+
+    product_data = list(products.values('id', 'name', 'price', 'stock'))
+    featured_data = list(featured.values('id', 'name', 'price', 'stock'))
+
+    return JsonResponse({
+        'products': product_data,
+        'featured': featured_data
+    })
+
+
+# AFTER: With custom manager
+def product_list(request):
+    products = Product.items.available().order_by('-created_at')
+    featured = Product.items.featured()
+
+    product_data = list(products.values('id', 'name', 'price', 'stock'))
+    featured_data = list(featured.values('id', 'name', 'price', 'stock'))
+
+    return JsonResponse({
+        'products': product_data,
+        'featured': featured_data
+    })
